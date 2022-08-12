7ac445fde3e37272d0d9eaac6f485cf2aac452ee
Jordan Janakievski <jordanjanakievski@Jordans-MacBook-Pro.local>
2022-08-12 13:15:39 -0700

    Updated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Nav, Container } from "react-bootstrap";
 import "./Footer.css";
 import {
-	IoMail,
-	IoReader,
+	IoLogoGitlab,
 	IoLogoLinkedin,
 	IoLogoGithub,
 	IoLogoReact,
@@ -15,12 +14,9 @@ const Footer = () => {
 		<Container className={"footer"}>
 			<Container>
 				<Nav>
-					<Nav.Link href="mailto:jordan.a.janakievski@gmail.com">
-						<IoMail size={30} className={"logo"} />
+					<Nav.Link href="https://gitlab.com/jordanjanakievski">
+						<IoLogoGitlab size={30} className={"logo"} />
 					</Nav.Link>
-					<a href="jordan_janakievski_resume.pdf" download>
-						<IoReader size={30} className={"logo"} />
-					</a>
 					<Nav.Link href="https://www.linkedin.com/in/jordan-janakievski">
 						<IoLogoLinkedin size={30} className={"logo"} />
 					</Nav.Link>
@@ -29,16 +25,6 @@ const Footer = () => {
 					</Nav.Link>
 				</Nav>
 			</Container>
-			<p>
-				Built with <IoLogoReact size={20} color="#646669" />{" "}
-				<Nav.Link
-					href="https://github.com/jordanjanakievski/jordanjanakievski.github.io"
-					className="version"
-				>
-					<IoGitBranch size={20} />
-					v2.1.0
-				</Nav.Link>
-			</p>
 		</Container>
 	);
 };
