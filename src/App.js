e37d4bf9662ef426d42efce9ee58b831dc181ad3
Jordan Janakievski <jordanjanakievski@Jordans-MacBook-Pro.local>
2022-08-12 12:04:25 -0700

    Update color scheme and font selection of site

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
-import Experience from "./Pages/Experience/Experience";
-import Projects from "./Pages/Projects/Projects";
-import SelfEdu from "./Pages/SelfEdu/SelfEdu";
 import Page404 from "./Pages/404/404";
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
@@ -15,9 +12,6 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route exact path="/" element={<Homepage />} />
-					<Route path="/experience" element={<Experience />} />
-					<Route path="/projects" element={<Projects />} />
-					<Route path="/self-education" element={<SelfEdu />} />
 					<Route path="*" element={<Page404 />} />
 				</Routes>
 				<Footer />
