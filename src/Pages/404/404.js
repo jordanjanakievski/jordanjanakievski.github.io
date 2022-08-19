c5765687bbf4d15d717ae4825ef92e89ea304622
Jordan Janakievski <jordanjanakievski@Jordans-MacBook-Pro.local>
2022-08-19 11:57:30 -0700

    Updating 404 page and README

--- src/Pages/404/404.js
+++ src/Pages/404/404.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import "./404.css";
 
 function Page404() {
 	return (
 		<div className={"content"}>
 			<br />
-			<h2 id={"center"}>Whoops, 404 Error!</h2>
-			<h2 id={"center"}>It looks like that pages does not exist</h2>
+			<h2>Whoops, 404 Error!</h2>
+			<h2>It looks like that pages does not exist.</h2>
+			<div className={"back-home"}>
+				<Nav.Link href="/" id="decorations">
+					Back to Home
+				</Nav.Link>
+			</div>
 		</div>
 	);
 }
