70efbef7e6c8c040a0f548b181684d3fc57cde93
Jordan Janakievski <jajanak10@gmail.com>
2023-07-04 14:18:49 -0700

    Attempting to fix rendering bugs

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
             }
         ),
         paths: {
-            base: dev ? '' : process.env.BASE_PATH,
+            base: "/jordanjanakievski.github.io",
         }
     }
 };
