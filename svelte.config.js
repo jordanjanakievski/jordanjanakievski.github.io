164e2f35957a187bcc0fd22847ce153d27199ca4
Jordan Janakievski <jajanak10@gmail.com>
2023-07-04 13:46:03 -0700

    gh-pages config update

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
-export default {
+const config = {
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: false
-        })
+        adapter: adapter(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
     }
 };
