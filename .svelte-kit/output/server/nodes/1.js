a431c20a653a198481312c2e7aba1112f581a336
Jordan Janakievski <jajanak10@gmail.com>
2023-12-01 20:08:17 -0500

    Remove svelte kit directory

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.20007425.js","_app/immutable/chunks/index.dba1dfee.js","_app/immutable/chunks/singletons.5d1c318b.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.1516fc6d.js","_app/immutable/chunks/index.dba1dfee.js"];
-export const stylesheets = ["_app/immutable/assets/2.853bc628.css"];
-export const fonts = [];
